a66c38aac868d50c7b32b93209442e2b2a14ebc4
ochwo laban <labanochwo@gmail.com>
2025-06-09 22:49:58 +0300

    fixing utils import

--- finetune/textgen/gpt2/finetune_for_summarization.py
+++ finetune/textgen/gpt2/finetune_for_summarization.py
@@ -17,10 +17,6 @@ from transformers import (
     AutoModelForCausalLM,
 )
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
-
 from sum_data_collator import DataCollatorForSumLanguageModeling
 from sum_dataset import LineByLineSumTextDataset
 
